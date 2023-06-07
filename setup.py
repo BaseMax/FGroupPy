1d4400bac8d0975c253634861b9c1a9e94e4ca79
Max Base <MaxBaseCode@Gmail.Com>
2023-06-07 23:52:45 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='fgroup',
+    version='1.0.0',
+    py_modules=['fgroup'],
+    description='A software tool for working with mathematical groups',
+    author='Max Base',
+    author_email='maxbasecode@gmail.com',
+    url='https://github.com/basemax/fgroup',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
